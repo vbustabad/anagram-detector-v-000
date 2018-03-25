9604fc29dbb18a18fe39c7afbf2d0ab194fcf8ec
Veronica Bustabad <vbustabad@gmail.com>
2018-03-25 14:37:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
+require 'pry'
 # Your code goes here!
+
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_possible_anagrams)
+    array_of_possible_anagrams.select do |possible_anagram|
+      possible_anagram.split("").sort == @word.split("").sort
+    end
+  end
+
+end
