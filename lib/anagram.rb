b0f2bd8c76c7e883c0533a906dfd8e796d7d2352
Veronica Bustabad <vbustabad@gmail.com>
2018-03-25 14:22:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Your code goes here!
 
 class Anagram
@@ -11,6 +12,7 @@ class Anagram
   def match(array_of_possible_anagrams)
     array_of_possible_anagrams.select do |possible_anagram|
       if possible_anagram.split(/\w/).sort == @word.split(/\w/).sort
+        binding.pry
         possible_anagram
       else
         []
