f83552ced69bf82d6f5b1257802115f57523344d
Veronica Bustabad <vbustabad@gmail.com>
2018-03-25 14:17:19 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   def initialize(word)
     @word = word
   end
-  
+
   def match(array_of_possible_anagrams)
     array_of_possible_anagrams.select do |possible_anagram|
       possible_anagram.split(/\w/).sort == @word.split(/\w/).sort
