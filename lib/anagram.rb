1f6073cb9448a91692d9efce81dcd19df717457c
Veronica Bustabad <vbustabad@gmail.com>
2018-03-25 13:50:35 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_possible_anagrams)
+    array_of_possible_anagrams.select do |word|
+      if word.split(/\w/).sort == @word.split(/\w/).sort
+        return word
+      else
+        []
+      end
+    end
+  end
+
+end
